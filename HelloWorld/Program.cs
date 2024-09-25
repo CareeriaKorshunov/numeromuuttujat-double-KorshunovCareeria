dbbc2b8380baaec4586e97af025733b73e6260f4
KorshunovCareeria <158146367+KorshunovCareeria@users.noreply.github.com>
2024-09-25 10:53:26 +0300

    Update Program.cs

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,11 @@
         {
             //Koodi tähän
            
-
+double luku1 = 10.5f;
+            double luku2 = 5.5f;
+Console.WriteLine("Luvut yhdessä pötkössä: " + luku1+luku2);
+Console.WriteLine("Luvut erikseen listattuna: " + luku1+" "+luku2);
+Console.WriteLine("Lukujen summa on: " + (luku1+luku2));
 
         }
     }
